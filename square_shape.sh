3df1559de84063fff90401fc76d21343f269c4cb
Zayne Frabutt <zfrabutt@gmu.edu>
2020-12-01 19:16:37 +0000

    Fixed boxes to be blue

--- square_shape.sh
+++ square_shape.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun gazebo_ros spawn_model -file box.sdf -sdf -model $1 -y $3 -x $2 -z 1.0
+rosrun gazebo_ros spawn_model -file models/box_make.urdf -urdf -model $1 -y $3 -x $2 -z 5.0
